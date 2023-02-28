c4d9e9bdf78873e5c1f9b0ea7a124c024bfe9c0d
Saanykeys <125324312+Saanykeys@users.noreply.github.com>
2023-02-28 11:03:57 -0500

    Add files via upload

---
+++ example.py
@@ -0,0 +1,12 @@
+class Animal:
+    def_int_(self, name, energy)
+    self.name = name 
+    self.energy = energy
+    
+    def eat(self, units_of_food):
+        self.energy += units_of_food
+        print(f"{self.name} is eating {units_of_food} units of food. Its energy is now {self.energy}")
+        
+         def eat(self, units_of_food):
+        self.energy += units_of_food
+        print(f"{self.name} is eating {units_of_food} units of food. Its energy is now {self.energy}")
